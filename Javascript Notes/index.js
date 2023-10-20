c0e7a481e6b9cea6bb0dcb8b24fe24b472a5b9aa
DILIP SANAP <dilipinbsanap@gmail.com>
2023-10-20 16:17:04 +0530

    notes updated

--- Javascript Notes/index.js
+++ Javascript Notes/index.js
@@ -1,9 +1,40 @@
+// class ListNode {
+//   constructor(data) {
+//     this.data = data;
+//     this.next = null;
+//   }
+// }
 
-var arr=[1,2,3,4,5]
-for (var i = 0; i < arr.length; i++){
-    (function (i) {
-        setTimeout(function () {
-            console.log(arr[i], i);
-        },i*1000)
-    })(i)
+// var linkedList =function(arr) {
+//   let head = null;
+//   let tail = null;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let newNode = new ListNode(arr[i]);
+//     if (head === null) {
+//       head = newNode;
+//       tail = newNode;
+//     } else {
+//       tail.next = newNode;
+//       tail = newNode;
+//     }
+//   }
+
+//   return head;
+// }
+
+// var arr = [1, 2, 3, 4, 5];
+// console.log(linkedList(arr));
+
+function greet(greetings,city) {
+    console.log(`${greetings}, ${this.name} from ${city}`);
 }
+perosn = {
+    name:"Dilip"
+}
+
+greet.call(perosn, "Hello", "Nashik");
+greet.apply(perosn, ["Hiii..", "Pune"]);
+const greetperson = greet.bind(perosn, "Namaste", "Delhi");
+
+greetperson()
